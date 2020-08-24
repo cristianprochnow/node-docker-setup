9776d7acf82422545dec7ce607cc71263816c068
cristianprochnow <cristianprochnow@gmail.com>
2020-08-23 22:49:37 -0300

    feat: add basic Node setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const PORT = 3333
+const HOST = '0.0.0.0'
+
+const app = express()
+
+app.get('/', (request, response) => response.send('Hello, World!'))
+
+app.listen(PORT, HOST)
